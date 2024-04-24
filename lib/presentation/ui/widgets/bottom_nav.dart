3424c20a3dea71647c7b95e7760188dbbfe0815b
Hassan <hasanahmad9812021@gmail.com>
2024-04-24 18:36:44 +0600

    main bottoms chat ui has been done

--- lib/presentation/ui/widgets/bottom_nav.dart
+++ lib/presentation/ui/widgets/bottom_nav.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:my_campus/presentation/ui/screens/teacher_screens/teacher_homePage/sub_pages/fac_batch_and_course_list_screen.dart';
+import '../../state_holders/faculty_state_holders/fac_creating_sub_grp_batch_sec_controller.dart';
+import '../../state_holders/faculty_state_holders/fac_show_group_batch_section_course_controller.dart';
+import '../screens/teacher_screens/teacher_homePage/sub_pages/fac_chat_screen.dart';
 import 'file_upload.dart';
 
 class BottomNav extends StatelessWidget {
@@ -41,25 +44,123 @@ class BottomNav extends StatelessWidget {
                     ],
                   ),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    Get.to(
-                      () => const FacBatchAndCourseListScreen(),
+                GetBuilder<FacShowGroupBatchSectionCourseController>(
+                  builder: (facShowGroupBatchSectionCourseController) {
+                    return GestureDetector(
+                      onTap: () async {
+                        final List<Map<String, String>> batchCoursePairs = [];
+
+
+                        final result = await facShowGroupBatchSectionCourseController.showGroups();
+                        if (result) {
+                          final dataList = facShowGroupBatchSectionCourseController.facultyCreatingSubGrpBatchSecDataList;
+
+                          for (final data in dataList!) {
+                            final a = data.batch!;
+                            final b = data.courseCode!;
+                            batchCoursePairs.add({'batch': a, 'courseCode': b});
+                          }
+                          //facultyBatchesAndCourses(context, batchCoursePairs);
+                        }
+
+                        showDialog(
+                          context: (context),
+                          builder: (context) {
+                            return StatefulBuilder(
+                              builder: (context, StateSetter setState) {
+                                return AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16.0),
+                                  ),
+                                  title: Center(
+                                    child: Text(
+                                      "My Groups",
+                                      style:
+                                      TextStyle(fontSize: 26.sp, fontWeight: FontWeight.w700),
+                                    ),
+                                  ),
+                                  actions: [
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 3.0.w),
+                                      child: Stack(
+                                        children: [
+                                          Container(
+                                            width: 380.w,
+                                            height: 380.h,
+                                            decoration: const BoxDecoration(
+                                              color: Color(0xFFF8FFAC),
+                                              borderRadius: BorderRadius.all(
+                                                Radius.circular(20),
+                                              ),
+                                            ),
+                                            child: ListView.separated(
+                                              itemCount: batchCoursePairs.length,
+                                              itemBuilder: (context, index) {
+                                                return InkWell(
+                                                  onTap: () async {
+                                                    Navigator.pop(context);
+                                                    Get.to(
+                                                          () => FacChatScreen(
+                                                        batch: batchCoursePairs[index]['batch'].toString(),
+                                                        section: batchCoursePairs[index]['batch'].toString(), // section hobe
+                                                        courseCode: batchCoursePairs[index]['courseCode'].toString(),
+                                                      ),
+                                                    );
+                                                  },
+                                                  child: ListTile(
+                                                    leading: Text(
+                                                      batchCoursePairs[index]['batch'] ?? 'Unknown',
+                                                      style: TextStyle(
+                                                        color: const Color(0xFF0D6858),
+                                                        fontWeight: FontWeight.w500,
+                                                        fontSize: 20.sp,
+                                                      ),
+                                                    ),
+                                                    title: Text(
+                                                      batchCoursePairs[index]['courseCode'] ?? 'Unknown',
+                                                      style: TextStyle(
+                                                        color: const Color(0xFF0D6858),
+                                                        fontWeight: FontWeight.w500,
+                                                        fontSize: 20.sp,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                );
+                                              },
+                                              separatorBuilder: (context, index) {
+                                                return const Divider(
+                                                  thickness: 1,
+                                                  indent: 10,
+                                                  endIndent: 10,
+                                                );
+                                              },
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        );
+                      },
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.mark_unread_chat_alt_rounded,
+                            size: 30.w,
+                            color: Colors.blue,
+                          ),
+                          Text(
+                            "Chat",
+                            style: TextStyle(fontSize: 16.sp, color: Colors.black),
+                          ),
+                        ],
+                      ),
                     );
-                  },
-                  child: Column(
-                    children: [
-                      Icon(
-                        Icons.mark_unread_chat_alt_rounded,
-                        size: 30.w,
-                        color: Colors.blue,
-                      ),
-                      Text(
-                        "Chat",
-                        style: TextStyle(fontSize: 16.sp, color: Colors.black),
-                      ),
-                    ],
-                  ),
+                  }
                 ),
                 GestureDetector(
                   onTap: () {
